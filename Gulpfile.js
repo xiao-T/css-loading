3d3f8fe260be3eee7a70d5cc608a84d63927645d
xiaoT <270199332@qq.com>
2015-10-30 17:38:09 +0800

    添加autoprefixer任务

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 	plumber = require('gulp-plumber'),
 	compass = require('gulp-compass'),
+	autoprefixer = require('gulp-autoprefixer'),
 	watch = require('gulp-watch'),
 	dir = {
 		'src': 'src',
@@ -10,14 +11,24 @@ var gulp = require('gulp'),
 gulp.task('watch', function(){
 	return gulp.watch(dir.src + '/scss/*.scss', ['compass']);
 })
+/**
+ * compass 
+ * 编译 sass
+ */
 gulp.task('compass', function(){
 	console.log('start compass')
 	gulp.src(dir.src + '/scss/*.scss')
 		.pipe(compass({
+			sourcemap: true,
 			css: dir.src + '/css',
 			style: 'compact',
 			sass: dir.src + '/scss'
 		}))
-		// .pipe(gulp.dest(dir.src + '/css'));
-		
 });
+gulp.task('autoprefixer', function(){
+	gulp.src(dir.src + '/css/*.css')
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions', 'IE 9']
+		}))
+		.pipe(gulp.dest(dir.src + '/css'))
+})
